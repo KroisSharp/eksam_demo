9832ff5bed1127002438d2a14672cf5dbbddd58a
Voodoo <itsme@dot.com>
2018-06-08 09:32:12 +0200

    commit

---
+++ app/src/main/java/com/example/voodoo/eksam_demo/MainActivity.java
@@ -0,0 +1,190 @@
+package com.example.voodoo.eksam_demo;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button StartKnap;
+    private ImageView StatusBillede;
+    public static final String TAG = "MainActivity";
+
+
+    GoogleApiClient googleApiClient = null;
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        StartKnap = findViewById(R.id.btn_Start);
+        StatusBillede = findViewById(R.id.pic_Billede);
+
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        Log.d(TAG, "Conencted to GoogleApiClient");
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                        Log.d(TAG, "Connection suspened");
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Log.d(TAG, "onConnectionFailed: connection failed");
+                    }
+                })
+                .build();
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1234); //1234 er request code
+    }
+
+    public void clk_Start(View view) {
+        Log.d(TAG, "clk_Start: Starter");
+        StartLocation();
+
+        StartGeofence(55.635367,12.089943, 250, "gul",2);
+        StartGeofence(55.637347,12.087529, 250, "vinder",1);
+        StatusBillede.setVisibility(View.VISIBLE);
+    }
+
+
+
+    private void StartLocation() {
+        Log.d(TAG, "StartLocation: start location called");
+        try {
+            LocationRequest locationRequest = LocationRequest.create()
+                    .setInterval(10000)//mili sek
+                    .setFastestInterval(5000)
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                Log.d(TAG, "StartLocation: error failed check");
+                return;
+            }
+
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    Log.d(TAG, "location updated lat/long" + location.getLongitude() + " " + location.getLatitude());
+                }
+            });
+        } catch (SecurityException e) {
+            Log.d(TAG, "StartLocation:" + e);
+        }
+    }
+
+
+
+    private void StartGeofence(double latitude, double longitude, float radius, String GEOFENCE_ID, int reqcode) {
+        Log.d(TAG, "StatGeofence: called");
+        try {
+            Geofence geofence = new Geofence.Builder()
+                    .setRequestId(GEOFENCE_ID) //butik id
+                    .setCircularRegion(latitude, longitude, radius)
+                    .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                    .setNotificationResponsiveness(1000)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                    .build();
+            GeofencingRequest geofencingRequest = new GeofencingRequest.Builder()
+                    .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                    .addGeofence(geofence).build();
+
+            Intent intent = new Intent(this, GeofenceSerivce.class);
+            PendingIntent pendingIntent = PendingIntent.getService(this, reqcode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            if (!googleApiClient.isConnected()) {
+                Log.d(TAG, "googleapi not ocnnected");
+            } else {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    Log.d(TAG, "StatGeofence: called check permission");
+                    return;
+                }
+                LocationServices.GeofencingApi.addGeofences(googleApiClient, geofencingRequest, pendingIntent)
+                        .setResultCallback(new ResultCallback<Status>() {
+                            @Override
+                            public void onResult(@NonNull Status status) {
+                                if (status.isSuccess()) {
+                                    Log.d(TAG, "added geofence ok");
+                                } else {
+                                    Log.d(TAG, "error failed to add geofence" + status);
+                                }
+                            }
+                        });
+            }
+
+
+        } catch (Exception e) {
+            Log.d(TAG, "StatGeofence: " + e);
+        }
+    }
+
+
+
+
+    @Override
+    public void onResume() {
+        Log.d(TAG, "onResume: called");
+        super.onResume();
+
+        int response = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+        if (response != ConnectionResult.SUCCESS) {
+
+            Log.d(TAG, "onResume: no google play");
+        } else {
+            Log.d(TAG, "onResume: no action req");
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: called");
+        super.onStart();
+        googleApiClient.reconnect();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: called");
+        super.onStop();
+        googleApiClient.disconnect();
+    }
+
+}
---
+++ app/src/main/java/com/example/voodoo/eksam_demo/vinderAcctivity.java
@@ -0,0 +1,13 @@
+package com.example.voodoo.eksam_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vinderAcctivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vinder_acctivity);
+    }
+}
